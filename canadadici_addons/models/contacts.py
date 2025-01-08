adb0b393ec66b514b29b01f41bc6de6572f2eb5a
KariSims <princesimbaalimasi@esp.sn>
2025-01-08 03:38:20 -0800

    num_phone and title_partner updated

--- canadadici_addons/models/contacts.py
+++ canadadici_addons/models/contacts.py
@@ -3,6 +3,16 @@ from odoo import fields, models, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    # titel = fields.Char('Vitre')
+    titel = fields.Selection([
+        ('', 'Choisissez un titre'),
+        ('mr', 'Mr'),
+        ('mme', 'Mme'),
+        ],
+        string="Ttitre",
+        required=True,
+        default='')
+    
     phone = fields.Char('phone')
     street2 = fields.Char('Street2')
 
