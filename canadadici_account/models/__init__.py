a7f07cf1a32b6e7629f72585879cfa4ea74524fb
KariSims <princesimbaalimasi@esp.sn>
2025-04-16 14:13:54 -0700

    canadadici_modules_v1.1__update

--- canadadici_account/models/__init__.py
+++ canadadici_account/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-from . import contacts
+from . import res_partner
 from . import payment_mode
 from . import account_move
 from . import account_move_line
