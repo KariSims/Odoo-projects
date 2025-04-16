7d1ca62acb25d0f4c703dde3d2b128639789d3de
Prince SIMBA <79909649+KariSims@users.noreply.github.com>
2025-04-16 05:00:23 -0700

    Delete jallow_addons directory

--- jallow_addons/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import account_move
-from . import sale_order
-from . import jallow_partner
-from . import jallow_society
-from . import pos_payment
-from . import pos_order
